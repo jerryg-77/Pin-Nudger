6952967f93d966e197daf5b554c82e2c3e24c217
jerryg-77 <114109436+jerryg-77@users.noreply.github.com>
2023-04-12 00:04:27 -0400

    Update Pin-Nudger.ino

--- Code/Pin-Nudger.ino
+++ Code/Pin-Nudger.ino
@@ -1,103 +1,95 @@
-/*
-    MPU6050 based Pinball Nudge Relay.
-    2022-12-31 by James R. Robinson (j2robin@gmail.com)
-    Uses the MPU6050 library from Henrique Bruno Fantauzzi de Almeida which was forked from the original library from Korneliusz Jarzebski
-    GIT: https://github.com/SrBrahma/Arduino-MPU6050
-    
-*/
+// Testing USB joystick function using Arduino Leonardo
+// using MPU6050 as analog x, y and z axis input
+// Code written by Pin Chung  
+// Sep 22, 2020
 
+#include <Joystick.h>
+#include <MPU6050_tockn.h>
 #include <Wire.h>
-#include <MPU6050.h>
 
-MPU6050 mpu;
+MPU6050 mpu6050(Wire);
+//Joystick_ Joystick;
 
-const float XNUDGE_FORCE = .6;
-const float YNUDGE_FORCE = .8;
-const unsigned int TIME_BETWEEN_PRESSES = 10;
-const unsigned int PRESS_DURATION = 10;
-const unsigned int DLPF_MODE = 6;
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_GAMEPAD, 0, 0,
+  true, true, false, 
+  false, false, false,
+  false, false, 
+  false, false, false);
 
-unsigned long lastTime;
-unsigned long currentTime;
-bool leftNudge, rightNudge, forwardNudge;
-int ON = 0, OFF = 1;
-Vector normAccel, newAccel;
+#define ResetPin 13
+#define Joystick1Pin 9
 
-void setup() 
-{
-     //Wait until the MPU6050 is up
-    while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-    {
-        delay(500);
-    }
-    
-    mpu.resetDevice(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G); 
+int lastButtonState=0;
+int currentButtonState;
 
-    mpu.calibrateAccelerometer();
+float zeroX=0;
+float zeroY=0;
+float zeroZ=0;
 
-    // Activate the low pass filter.  This is important to filter out things like vibrations from flipper force feedback and plunger use
-    mpu.setDLPFMode(DLPF_MODE);
+const float XNUDGE_FORCE = .4;
+const float YNUDGE_FORCE = .4;
 
-    currentTime = 0;
-    lastTime = 0;
-    leftNudge = false;
-    rightNudge = false;
-    forwardNudge = false;
+//upper and lower limit on the angle of the MPU6050 board range.
+int upperLimit = 45;
+int lowerLimit = -45;
+
+void setup() {
+
+  
+  Serial.begin(9600);
+  Joystick.begin();
+
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(false); //This is is not a necessary call. works fine without it.
+
 }
 
-void loop()
+void ResetMPU6050(float x, float y, float z){
+  //Center the Joystick at current position
+  zeroX=x;
+  zeroY=y;
+
+}
+
+void loop() 
 {
+  mpu6050.update();
+   float x=(mpu6050.getAccX());
+   float y=(mpu6050.getAccY());
 
-    normAccel.XAxis = 0;
-    normAccel.YAxis = 0;
-    normAccel.ZAxis = 0;
+  
+  x-=zeroX;
+  y-=zeroY;
+  
+/*   if (x>upperLimit) x=upperLimit;
+   else if (x<lowerLimit) x=lowerLimit;
+   if (y>upperLimit) y=upperLimit;
+   else if (y<lowerLimit) y=lowerLimit;
+*/
 
-    newAccel = mpu.readNormalizedAccelerometer();
 
-    normAccel.XAxis = newAccel.XAxis;
-    normAccel.YAxis = newAccel.YAxis;
-    normAccel.ZAxis = newAccel.ZAxis;
+ // Map the angles of x, y and z to joystick position 0 to 1024
+ // to reverse the axis, switch the last 2 numbers in the map() function
+ if (x < XNUDGE_FORCE && x > (-1* XNUDGE_FORCE)){
+    x = 0;
+ }
+  if (y < YNUDGE_FORCE && y > (-1 * YNUDGE_FORCE)) {
+    y = 0;
+ }
+   x = x *1000;
+   y = y *1000;
+   Joystick.setXAxis(map(x, lowerLimit,upperLimit, 1024,0));
+   Joystick.setYAxis(map(y, lowerLimit,upperLimit, 0, 1024));
+   if (x != 0) {
+        Serial.print ("\n x=");
+        Serial.print (x);
+   }
+   if (y != 0) {
+        Serial.print(" y=");
+        Serial.print (y);
+   }
 
-    currentTime = millis();
-
-    //All nudges have a TIME_BETWEEN_PRESSES value to prevent buttons triggering in rapid succession which could reult in a tilt in game
-
-    //Left Nudge
-    if (normAccel.XAxis>XNUDGE_FORCE && ((currentTime-lastTime)>TIME_BETWEEN_PRESSES)) {
-      if (!rightNudge) { //Don't trigger a left press if the right press is already happening
-        Serial.print ("Right");
-        leftNudge = true;
-        lastTime = millis();
-      }
-    } else {
-      if (leftNudge && (currentTime-lastTime > PRESS_DURATION)) { // Stop pressing the button after the appropiate time
-        leftNudge = false;
-        digitalWrite(4, OFF);
-      }  
-    }
-
-    //Right Nudge
-    if (normAccel.XAxis<(-1.0 * XNUDGE_FORCE) && ((currentTime-lastTime)>TIME_BETWEEN_PRESSES)) {
-      if(!leftNudge) { //Don't trigger a right press if the left press is already happening
-        Serial.print ("Left");
-        rightNudge = true;
-        lastTime = millis();
-      }
-    } else {
-      if (rightNudge && (currentTime-lastTime > PRESS_DURATION)) { // Stop pressing the button after the appropiate time
-        rightNudge = false;
-      }
-    }
-
-    //Trigger forward nudge from either forward or backward motion
-    if ((normAccel.YAxis<(-1.0 * YNUDGE_FORCE))||(normAccel.YAxis>YNUDGE_FORCE) && ((currentTime-lastTime)>TIME_BETWEEN_PRESSES)) {
-      Serial.print ("Front");
-      forwardNudge = true;
-      lastTime = millis();
-    } else {
-      if (forwardNudge && (currentTime-lastTime > PRESS_DURATION)) { // Stop pressing the button after the appropiate time
-        forwardNudge = false;
-      }
-    }
 
 }
